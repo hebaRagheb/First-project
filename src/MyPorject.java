846aa5ab478b76822412e651e9e256ebb7afbe29
Heba Abu Al-Ragheb <hebaar@q-pros.com>
2023-02-05 18:32:26 +0300

    project

--- src/MyPorject.java
+++ src/MyPorject.java
@@ -1,15 +1,85 @@
+import java.util.Date;
 
 public class MyPorject {
 
+	
+
+	@SuppressWarnings("deprecation") // added when using get.hour
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		String Myname = "heba";
-		System.out.println(Myname);
+	// 
 		
-		
-		
-		
+	String name = "heba";
+	
+	//name.contains("h")
+	//System.out.println(name.contains("h"));
+	
+	//name.charAt(1)
+	//System.out.println(name.charAt(1));
+	
+	//System.out.println(name.toUpperCase().charAt(1));
+	
+	//System.out.println(name.concat(" test"));
+	
+	//String zz = name+" "+"test";
+	//System.out.println(zz);
+	
+	//double myDouble = 88.3;
+   
+	//Math.PI;
+   // System.out.println(Math.PI);
+    
+   // System.out.println(Math.round(myDouble));
+    //System.out.println(Math.ceil(myDouble));
+    //System.out.println(Math.floor(myDouble));
+    
+    
+   // double myDoublee = -88.9;
+    //System.out.println(Math.floor(myDoublee));
+	
+   // System.out.println(Math.absExact((int) myDoublee));
+	
+	
+	//boolean test = false;
+	//System.out.println(test);
+	
+	
+	//Date mytime = new Date ();
+	//mytime.getHours();
+	//System.out.println(mytime.getHours());
+	
+	// ---------------Part 2 -------------------
+	
+//	for (int i=0; i<=100 ; i++) {
+//		
+//		//if (i%2!=0) {
+//			 if (i%2==1) {
+//			//System.out.println(i);
+//			if (i==9 || i==29 || i==39) {
+//				continue; // same as skip
+//				// break;     // go out of the loop
+//			}
+//			 System.out.println(i);
+//		} 	
+//	}
+	
+	
+	
+//	int i = 3;
+// while (i == 3) {
+//	 System.out.println(i);
+//	i++;
+//	}
+// System.out.println(i);
+//	}
+	
+	
+	//------------------------- part 3 ------------
+	// oop, inheretance, object 
+	
+	
+	
+	
 	}
-
 }
