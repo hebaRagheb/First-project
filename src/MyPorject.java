ede5ced352af586a2e56cd27eee5057c88cb1539
Heba Abu Al-Ragheb <hebaar@q-pros.com>
2023-02-05 17:12:22 +0300

    update

---
+++ src/MyPorject.java
@@ -0,0 +1,15 @@
+
+public class MyPorject {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String Myname = "heba";
+		System.out.println(Myname);
+		
+		
+		
+		
+	}
+
+}
